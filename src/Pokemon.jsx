a7e55b20629bd28c3091a7b9d212f58ea671ef3a
HP <zuhairsaleem343@gmail.com>
2025-04-15 21:55:38 +0500

    search functionality is completed

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,64 +1,84 @@
-import { useEffect, useState } from "react"
-import "./index.css"
-import { PokemonCard } from "./PokemonCard"
-export const Pokemon =()=>{
-    const [pokemon, setPokemon]=useState([])
-    const [loading, setLoading]=useState(true)
-    const [error, setError] = useState("")
-    const API = "https://pokeapi.co/api/v2/pokemon?limit=50"
-    const fetchPokemon= async ()=>{
+import { useEffect, useState } from "react";
+import "./index.css";
+import { PokemonCard } from "./PokemonCard";
+export const Pokemon = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [search, setSearch] = useState("");
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=50";
+  const fetchPokemon = async () => {
+    try {
+      const res = await fetch(API);
+      const data = await res.json();
+      // console.log("total pokemons: ",data)
+      const detailedPokemonData = data.results.map(async (currPokemon) => {
+        // console.log(currPokemon.url)
         try {
-            const res = await fetch(API)
-            const data = await res.json()
-            // console.log("total pokemons: ",data)
-            const detailedPokemonData=data.results.map( async(currPokemon)=>{
-                // console.log(currPokemon.url)
-                try {
-                    const res = await fetch(currPokemon.url)
-                    const data = await res.json()
-                    // console.log("URLS: ",data)
-                    return data;
-                } catch (error) {
-                    setError(error)
-                }
-            })
-            // console.log("detailedPokemonData: ",detailedPokemonData)
-            const detailedRespones = await Promise.all(detailedPokemonData)
-            console.log("detailed Respons: ", detailedRespones)
-            setPokemon(detailedRespones)
-            setLoading(false)
+          const res = await fetch(currPokemon.url);
+          const data = await res.json();
+          // console.log("URLS: ",data)
+          return data;
         } catch (error) {
-            setError(error);
-            setLoading(false)
+          setError(error);
         }
+      });
+      // console.log("detailedPokemonData: ",detailedPokemonData)
+      const detailedRespones = await Promise.all(detailedPokemonData);
+      console.log("detailed Respons: ", detailedRespones);
+      setPokemon(detailedRespones);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
     }
-    useEffect(()=>{
-        fetchPokemon()
-    },[])
-    if(loading){
-        return <div>
-            <h1>Loading...</h1>
+  };
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+  const searchData = pokemon.filter((curPokemon) =>
+    curPokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <h1>Error: {error.message}</h1>
+      </div>
+    );
+  }
+  return (
+    <>
+      <section className="container">
+        <header>
+          <h1>Lets catch pokemon</h1>
+        </header>
+        <div className="pokemon-search">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search Pokemon"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
-    }
-    if(error){
-        return <div>
-            <h1>Error: {error.message}</h1>
+        <div>
+          <ul className="cards">
+            {searchData.map((curPokemon) => {
+              return (
+                <PokemonCard key={curPokemon.id} curPokemon={curPokemon} />
+              );
+            })}
+          </ul>
         </div>
-    }
-    return <>
-        <section className="container">
-            <header>
-                <h1>Lets catch pokemon</h1>
-            </header>
-            <div>
-                <ul className="cards">
-                    {
-                        pokemon.map((curPokemon)=>{
-                            return <PokemonCard key={curPokemon.id} curPokemon={curPokemon}/>
-                        })
-                    }
-                </ul>
-            </div>
-        </section>
+      </section>
     </>
-}
+  );
+};
